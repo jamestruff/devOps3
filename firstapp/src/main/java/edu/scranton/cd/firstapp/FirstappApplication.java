81959b87caa14fa8f1948e7e96049a83854ab574
James Ruff <james.ruff@scranton.edu>
2020-05-12 18:38:23 -0400

    Second commit and push

--- firstapp/src/main/java/edu/scranton/cd/firstapp/FirstappApplication.java
+++ firstapp/src/main/java/edu/scranton/cd/firstapp/FirstappApplication.java
@@ -1,13 +1,20 @@
 package edu.scranton.cd.firstapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstappApplication {
+public class FirstappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstappApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	return application.sources(FirstappApplication.class);
 	}
 
 }
